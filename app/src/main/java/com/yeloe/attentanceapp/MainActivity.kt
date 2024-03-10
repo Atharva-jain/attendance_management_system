af71e811def59a576394d72dcaf94cd5fd36bce5
atharva-jain <atharvajain01@gmail.com>
2024-03-10 15:41:53 +0530

    initial commit

---
+++ app/src/main/java/com/yeloe/attentanceapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yeloe.attentanceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
